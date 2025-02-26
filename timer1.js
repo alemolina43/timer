468ae618b5651a8f1ecd2ff1b608af394af990f3
Alejandra Gutierrez <alexmarket43@gmail.com>
2025-02-25 19:45:12 -0800

    Implement a program that takes command line arguments and sets a timer for each valid input

--- timer1.js
+++ timer1.js
@@ -1,25 +1,16 @@
-const readLine = require('readline');
+const args = process.argv.slice(2);
 
-const rl = readLine.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
 
-rl.question('Please set amount of time (in secs)', (seconds) => {
-  const secsToArr = seconds.split(" ").map(s => Number(s)).filter(s => !isNaN(s) && s >= 0);
-      
-  if (secsToArr.length <= 0) {
-    rl.close();
-    return;
-  }
+const seconds = args.map(s => Number(s)).filter(s => !isNaN(s) && s >= 0);
 
-  for (const sec of secsToArr) {
-    const delay = sec * 1000;
-    setTimeout(() => {
-      process.stdout.write(".\n");
-    }, delay);
-  }
+if (seconds.length <= 0) {
+  return;
+}
 
-  rl.close();
-});
+for (const sec of seconds) {
+  const delay = sec * 1000;
+  setTimeout(() => {
+    process.stdout.write(".\n");
+  }, delay);
+}
 
